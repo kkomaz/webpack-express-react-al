57afda200ef89584650d404c669f19b2cb45d885
kkomaz <leeajl006@gmail.com>
2016-04-14 18:04:17 -0400

    Add sass-loader packages
    - TODO: Add autoprefixer, preloader, post-css in future

--- webpack.config.js
+++ webpack.config.js
@@ -13,11 +13,17 @@ module.exports = {
     filename: 'bundle.js',
   },
   module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      loaders: ['react-hot', 'babel'],
-    }],
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ['react-hot', 'babel'],
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass'],
+      },
+    ],
   },
   resolve: {
     extensions: ['', '.js', '.jsx'],
