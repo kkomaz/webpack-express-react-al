1d68dd3aeae1075593dfdc445853a7e69aa1c3ad
kkomaz <leeajl006@gmail.com>
2016-04-23 15:38:46 -0400

    Add store and configure redux dev tools

--- src/main.js
+++ src/main.js
@@ -6,14 +6,15 @@ import thunk from 'redux-thunk';
 import reducers from './reducers';
 
 import AppContainer from './containers/app_container';
+import configureStore from './store/configureStore';
 
 // Sass Stylesheet
 import './stylesheets/style.scss';
 
 
-const store = applyMiddleware(thunk)(createStore);
+const store = configureStore();
 
 ReactDOM.render(
-  <AppContainer store={store(reducers)} />,
+  <AppContainer store={store} />,
   document.querySelector('.container')
 );
